9774ab4a114c4ed09e08a25d9676e01698ba4156
Peng Yue <peng.yue@sparksoft.uk>
2021-05-16 21:20:13 +0100

    Restrucutre the code

--- autonomous_car/object_detection/run_openvino.sh
+++ autonomous_car/object_detection/run_openvino.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python webstreaming_openvino.py --ip 0.0.0.0 --port 8080 --prototxt training/MobileNetSSD_deploy.prototxt --model training/MobileNetSSD_deploy.caffemodel
+python webstreaming_openvino.py --ip 0.0.0.0 --port 8080 --prototxt training_model/MobileNetSSD_deploy.prototxt --model training_model/MobileNetSSD_deploy.caffemodel
